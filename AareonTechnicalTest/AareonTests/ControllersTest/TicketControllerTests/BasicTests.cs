72f22aa5b72c611bb07d2306fa7a569580c3d34c
dapple16 <dapple@live.com>
2022-03-07 17:37:55 +0000

    Initial Test project.

---
+++ AareonTechnicalTest/AareonTests/ControllersTest/TicketControllerTests/BasicTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace AareonTests.ControllersTest.TicketControllerTests
+{
+	public class BasicTests
+	{
+		[Fact]
+		public void TicketController_CanInitialise()
+		{
+			var ticketController = new TicketController();
+
+			Assert.NotNull(ticketController);
+		}
+	}
+}
