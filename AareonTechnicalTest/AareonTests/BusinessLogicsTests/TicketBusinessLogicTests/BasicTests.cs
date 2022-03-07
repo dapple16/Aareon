a3e3c752fd978ccf278273ae44c853edfe884373
dapple16 <dapple@live.com>
2022-03-07 18:58:08 +0000

    Added more tests, build is not complete.

---
+++ AareonTechnicalTest/AareonTests/BusinessLogicsTests/TicketBusinessLogicTests/BasicTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace AareonTests.BusinessLogicsTests.TicketBusinessLogicTests
+{
+	public class BasicTests
+	{
+		[Fact]
+		public void SmokeTest()
+		{
+			Assert.True(true);
+		}
+
+		[Fact]
+		public void TicketBlConstructorInitialised()
+		{
+			var sut = new TicketBL();
+			Assert.IsAssignableFrom<ICrudBLProvider>(sut);
+		}
+	}
+}
---
+++ AareonTechnicalTest/AareonTests/ControllersTests/TicketControllerTests/BasicTests.cs
@@ -0,0 +1,61 @@
+﻿using AareonTechnicalTest.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using Moq;
+
+namespace AareonTests.ControllersTest.TicketControllerTests
+{
+	public class BasicTests
+	{
+		[Fact]
+		public void CanInitialise()
+		{
+			var sut = new TicketController();
+
+			Assert.NotNull(sut);
+		}
+
+		[Fact]
+		public void Get_returns_Ok()
+		{
+			var crudBlMock = Mock.Of<ICrudBLProvider>();
+			var sut = new TicketController(crudBlMock);
+			var result = sut.Get();
+
+			_ = Assert.IsAssignableFrom<IActionResult>(result);
+		}
+
+		[Fact]
+		public void Put_returns_Ok()
+		{
+			var crudBlMock = Mock.Of<ICrudBLProvider>();
+			var sut = new TicketController(crudBlMock);
+			var ticketModel = new TicketModel();
+			var result = sut.Put(ticketModel);
+
+			_ = Assert.IsAssignableFrom<IActionResult>(result);
+		}
+
+		[Fact]
+		public void Create_returns_Ok()
+		{
+			var crudBlMock = Mock.Of<ICrudBLProvider>();
+			var sut = new TicketController(crudBlMock);
+			var ticketModel = new TicketModel();
+			var result = sut.Create(ticketModel);
+
+			_ = Assert.IsAssignableFrom<IActionResult>(result);
+		}
+
+		[Fact]
+		public void Delete_returns_Ok()
+		{
+			var crudBlMock = Mock.Of<ICrudBLProvider>();
+			var sut = new TicketController(crudBlMock);
+			var ticketId = 1;
+			var result = sut.Delete(ticketId);
+
+			_ = Assert.IsAssignableFrom<IActionResult>(result);
+		}
+	}
+}
