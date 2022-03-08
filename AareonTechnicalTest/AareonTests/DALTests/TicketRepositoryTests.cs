e352525ea037c711e96b821cade2d83ca5071d07
dapple16 <dapple@live.com>
2022-03-08 21:34:12 +0000

    TicketRepository Test, doesn't build yet.

--- AareonTechnicalTest/AareonTests/DALTests/TicketRepositoryTests.cs
+++ AareonTechnicalTest/AareonTests/DALTests/TicketRepositoryTests.cs
@@ -7,10 +7,30 @@ namespace AareonTests.DALTests
 {
 	public class TicketRepositoryTests
 	{
+		private TicketRepository _repo;
+
+		public TicketRepositoryTests()
+		{
+			_repo = new TicketRepository();
+		}
+
+		[Fact]
+		public async void CanAddNewItemToTicketRepository()
+		{
+			var ticket = new Ticket
+			{
+				PersonId = 1,
+				Content = "My Content"
+			};
+
+			var result = await repo.Add(ticket);
+
+			Assert.IsAssignableFrom<bool>(result);
+		}
+
 		[Fact]
 		public async void test()
 		{
-			var repo = new TicketRepository();
 			var result = await repo.FindById(1);
 
 			Assert.IsAssignableFrom<IEnumerable<Ticket>>(result);
