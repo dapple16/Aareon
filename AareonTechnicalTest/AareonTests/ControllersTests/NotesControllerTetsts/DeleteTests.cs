dcc9a92686a5cda0882706ea1894ff8688eb643a
dapple16 <dapple@live.com>
2022-03-10 19:25:34 +0000

    More Tests on note

---
+++ AareonTechnicalTest/AareonTests/ControllersTests/NotesControllerTetsts/DeleteTests.cs
@@ -0,0 +1,65 @@
+﻿using AareonTechnicalTest.BL;
+using AareonTechnicalTest.BL.Models;
+using AareonTechnicalTest.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AareonTests.ControllersTests.NotesControllerTetsts
+{
+	public class DeleteTests
+	{
+		private Mock<ICrudBLProvider<NoteModel>> _crudBlMock;
+		private NoteController InitialiseConstructor()
+		{
+			var sut = new NoteController(_crudBlMock.Object);
+			return sut;
+		}
+
+		[Fact]
+		public void Delete_returns_Ok()
+		{
+			_crudBlMock = new Mock<ICrudBLProvider<NoteModel>>();
+			NoteController sut = InitialiseConstructor();
+			var result = sut.Delete(0).Result;
+
+			_ = Assert.IsAssignableFrom<IActionResult>(result);
+		}
+
+		[Fact]
+		public void Delete_VerifyCrudProviderIsCalled()
+		{
+			_crudBlMock = new Mock<ICrudBLProvider<NoteModel>>();
+			NoteController sut = InitialiseConstructor();
+			var result = sut.Delete(1).Result;
+			_crudBlMock.Verify(v => v.Delete(It.IsAny<int>()), Times.Once);
+		}
+
+
+		[Fact]
+		public void Delete_returnsTrueOnSuccess()
+		{
+			_crudBlMock = new Mock<ICrudBLProvider<NoteModel>>();
+			_crudBlMock.Setup(s => s.Delete(1)).Returns(Task.FromResult(true));
+			NoteController sut = InitialiseConstructor();
+
+			var result = sut.Delete(1).Result as OkObjectResult;
+
+			_ = Assert.IsAssignableFrom<bool>(result.Value);
+		}
+
+		[Fact]
+		public async void Delete_returnsNoContentOnFail()
+		{
+			_crudBlMock = new Mock<ICrudBLProvider<NoteModel>>();
+			_crudBlMock.Setup(s => s.Delete(1)).ThrowsAsync(new Exception());
+			NoteController sut = InitialiseConstructor();
+
+			var result = await sut.Delete(1);
+
+			_ = Assert.IsAssignableFrom<NoContentResult>(result);
+		}
+	}
+}
